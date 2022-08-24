250add5610f6a5d5afbfa192fa1dc5a89f4880f8
vladtisa <tisavlad00@gmail.com>
2022-08-24 15:00:31 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+a=input("enter a")
+b=input("enter b")
+c=input("enter c")
+if a > b :
+        print("Good")
+elif a<b :
+        print("try again")
+elif a==b :
+ if b > c:
+        print("try again")
+
+elif b < c:
+        print("Good")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
